957624b83d38a94e4ce529f0a3ebb866d61bc488
sr0435 <45365011+sr0435@users.noreply.github.com>
2019-11-02 14:23:07 -0400

    Add files via upload
    a program to make a JSON file for azure to read

---
+++ jsonFileMaker.py
@@ -0,0 +1,21 @@
+import json
+
+data = {}
+data['people'] = []
+data['people'].append({
+    'name': 'Scott',
+    'website': 'stackabuse.com',
+    'from': 'Nebraska'
+})
+
+with open('data.txt','w') as outfile:
+    json.dump(data, outfile)
+
+
+with open('data.txt') as json_file:
+    data = json.load(json_file)
+    for p in data['people']:
+        print('Name: ' + p['name'])
+        print('Website: ' + p['website'])
+        print('From: ' + p['from'])
+        print('')
