8a5131415e0a73db70f367461c2f88dd5bc5c4be
sr0435 <45365011+sr0435@users.noreply.github.com>
2019-11-02 14:22:24 -0400

    Add files via upload

--- clubInfoWebScraping.py
+++ clubInfoWebScraping.py
@@ -1,23 +1,33 @@
 import os, bs4, requests
 import pandas as pd
 
-PATH = os.path.join("C:\\","Users","xxx","Documents","py") # you need to change to your local path
-res = pd.DataFrame()
-url = "http://bank-code.net/country/FRANCE-%28FR%29/"
-counter = 0
-
 def table_to_df(table): 
   return pd.DataFrame([[td.text for td in row.findAll('td')] for row in table.tbody.findAll('tr')])
 
 def next_page(soup): 
   return "http:" + soup.find('a', attrs={'rel':'next'}).get('href')
 
-while True:
-  print(counter)
-  page = requests.get(url)
-  soup = bs4.BeautifulSoup(page.content, 'lxml')
-  table = soup.find(name='table', attrs={'id':'tableID'})
-  res = res.append(table_to_df(table))
-  res.to_csv(os.path.join(os.path.join(PATH,"table.csv")), index=None, sep=';', encoding='iso-8859–1')
-  url = next_page(soup)
-  counter += 1
+def main():
+    PATH = os.path.join("C:\\","Users","Documents","OSU","HackOhio2019") # you need to change to your local path
+    res = pd.DataFrame()
+    url = "http://bank-code.net/country/FRANCE-%28FR%29/"
+    counter = 0
+    while True:
+        if counter == 3:
+            break
+        print(counter)
+        page = requests.get(url)
+        soup = bs4.BeautifulSoup(page.content, 'lxml')
+        table = soup.find(name='table', attrs={'id':'tableID'})
+        if table is None:
+            print("no table 'tableID' found for url {}".format(url))
+            #print("html content:\n{}\n".format( page.content))
+            #counter += 1
+        else:
+            res = res.append(table_to_df(table))
+            res.to_csv(os.path.join(os.path.join(PATH,"clubInfo.csv")), index=None, sep=';', encoding='UTF-8')
+            url = next_page(soup)
+        counter += 1
+
+if __name__ == "__main__":
+    main()
